c31016b717ae1202380123aeb0b55e71ae5c7b74
unknown <sacali99@gmail.com>
2020-10-24 17:16:36 -0500

    creacion de login_register.html

---
+++ js/js.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+ 	$("#Login").click(function() {
+		$("#g2").hide("slow");
+		$("#g1").show("slow");
+		$("#btn").css("left","0");
+		$("#btn").css("right","40.99%");
+	});
+	$("#Register").click(function() {
+		$("#g1").hide("slow");
+		$("#g2").show("slow");
+		$("#btn").css("right","0");
+		$("#btn").css("left","40.99%");
+	});
+});
+
