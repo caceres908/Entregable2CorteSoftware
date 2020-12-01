4354563fb3d846d9272ed3e0dbc39781c7d1cb16
unknown <sacali99@gmail.com>
2020-12-01 10:30:03 -0500

    agrego la url del server para poder traer los datos

--- js/aretes.js
+++ js/aretes.js
@@ -2,19 +2,19 @@ url = 'http://ec2-34-232-92-62.compute-1.amazonaws.com:5000';
 
 $('.special.cards .image').dimmer({
     on: 'hover'
-  });
+});
 
 var pro = []
 
-  var app = new Vue({
-      el: '#app',
-      data:{
-          productos: pro
-      },
-      created: function(){
-        this.get_datos()
-      },
-      methos:{
+var app = new Vue({
+    el: '#app',
+    data: {
+        productos: pro
+    },
+    created: function () {
+        this.get_datos();
+    },
+    methos: {
         get_datos: function () {
             axios.get(url + '/Aretes')
                 .then(response => {
@@ -23,5 +23,5 @@ var pro = []
                 })
                 .catch(error => console.error(error));
         }
-      }
-  })
+    }
+})
