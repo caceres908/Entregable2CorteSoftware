fcd68d6e1c98070077814d2c20f60ee077c86384
unknown <sacali99@gmail.com>
2020-12-01 23:50:21 -0500

    creo los metodos para taer a otros

---
+++ js/otros.js
@@ -0,0 +1,38 @@
+url = 'http://ec2-34-232-92-62.compute-1.amazonaws.com:5000';
+
+var pro = []
+var arete = {}
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        productos: pro,
+        producto: arete
+    },
+    created: function () {
+        this.get_datos();
+    },
+    methods: {
+        get_datos: function () {
+            axios.get(url + '/Otros')
+                .then(response => {
+                    this.productos = response.data.results;
+                    console.log('Get list Products', this.productos);
+                })
+                .catch(error => console.error(error));
+        },
+        arete_unico: function (u) {
+            document.getElementById('caja1').innerHTML = "";
+            axios.get(url +'/Otro/'+u.Codigo)
+                .then(response => {
+                    this.producto = response.data.results;
+                    console.log('Get list Products', this.producto);
+                    document.getElementById('caja1').innerHTML = "<div>" + "Nombre: "
+                        + this.producto.Nombre + "<br>" + "Descripcion: " + this.producto.Descripcion + "<br>" +
+                        "<img src= '" + this.producto.Imagen + "'>" + "<br>" + "Precio: " + this.producto.Precio +
+                        "</div>";
+                })
+                .catch(error => console.error(error));
+        }
+    }
+})
